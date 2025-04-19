edb18552cc3d199ea310615757df14c4dcc39838
lokyajadhav <lokya57@gmail.com>
2025-04-19 17:28:32 +0000

    Apr 19, 2025, 10:58 PM

--- src/app/dashboard/sidebar/nav-data.ts
+++ src/app/dashboard/sidebar/nav-data.ts
@@ -1,24 +1,24 @@
 export const navbarData = [
-  // {
-  //     routeLink: 'dashboard',
-  //     icon: 'fal fa-home',
-  //     label: 'Dashboard'
-  // },
   {
-    routeLink: 'chart',
+    routeLink: 'login',
     icon: 'fa fa-pie-chart',
-    label: 'Chart'
+    label: 'Task View'
   },
   {
-    routeLink: 'food-list',
+    routeLink: 'task-view',
+    icon: 'fa fa-pie-chart',
+    label: 'Task View'
+  },
+  {
+    routeLink: 'tasks-manager',
     icon: 'fa fa-list-ul',
-    label: 'food list'
+    label: 'Task Manager'
   },
   {
 
     routeLink: 'today',
     icon: ' fa fa-history',
-    label: 'Today'
+    label: 'Resources'
   },
 
 
