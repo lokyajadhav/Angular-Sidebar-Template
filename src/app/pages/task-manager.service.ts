add85f7dfde0cd6f3f25e0b049ea3a0a4d8d8173
lokyajadhav <lokya57@gmail.com>
2025-04-19 18:25:03 +0000

    Apr 19, 2025, 11:55 PM

--- src/app/pages/task-manager.service.ts
+++ src/app/pages/task-manager.service.ts
@@ -7,10 +7,23 @@ import { Observable } from 'rxjs';
 })
 export class TaskManagerService {
 
-  
+  private loggedIn = false;
   private apiUrl = 'http://localhost:8080';
 
   constructor(private http: HttpClient) {}
+
+
+  loggedInn(): void {
+    this.loggedIn = true;
+  }
+
+  logout(): void {
+    this.loggedIn = false;
+  }
+
+  isLoggedIn(): boolean {
+    return this.loggedIn;
+  }
 
   login(email: string, password: string, role: string): Observable<any> {
     const payload = { email, password };
