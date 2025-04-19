97edc4d15aaf4e24c1885d91053414f4e3f55c00
lokyajadhav <lokya57@gmail.com>
2025-04-19 18:30:25 +0000

    Apr 20, 2025, 12:00 AM

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
 import { TasksManangerComponent } from './pages/tasks-mananger/tasks-mananger.component';
 import { ResourcesComponent } from './pages/resources/resources.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AuthGuard } from './pages/auth.guard';
 
 
 
 
-const routes: Routes = [
-  { path: 'tasks-view', component: TaskViewComponent },
-  { path: 'tasks-manager', component: TasksManangerComponent },
-  { path: 'resources', component: ResourcesComponent }
-];
+
+  const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' }, // ⬅ Default route
+    { path: 'login', component: LoginComponent },
+    { path: 'tasks-view', component: TaskViewComponent, canActivate: [AuthGuard] },
+    { path: 'tasks-manager', component: TasksManangerComponent, canActivate: [AuthGuard] },
+    { path: 'resources', component: ResourcesComponent, canActivate: [AuthGuard] }
+  ];
+  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
