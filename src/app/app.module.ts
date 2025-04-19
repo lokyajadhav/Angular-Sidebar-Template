4ebd1e17a0ef7fe5c7622aafcf9b82324aa27efd
lokyajadhav <lokya57@gmail.com>
2025-04-19 17:15:44 +0000

    Apr 19, 2025, 10:45 PM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { SidebarComponent } from './dashboard/sidebar/sidebar.component';
-import { ChartpageComponent } from './pages/chart/chart.component';
+
 import { NgApexchartsModule } from "ng-apexcharts";
 import {RouterModule} from '@angular/router';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import { TodayComponent } from './pages/today/today.component';
-import { FoodListComponent } from './pages/food-list/food-list.component';
+
 import { MatListModule } from '@angular/material/list';
 import  { BodyComponent } from './body/body.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -30,9 +29,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     HeaderComponent,
     FooterComponent,
     SidebarComponent,
-    ChartpageComponent,
-    TodayComponent,
-    FoodListComponent,
+  
     BodyComponent
   ],
   imports: [
