d40c65409b072d3c823c8499ae23a2e851b32fc2
lokyajadhav <lokya57@gmail.com>
2025-04-19 18:35:29 +0000

    Apr 20, 2025, 12:05 AM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TaskManagerService } from './pages/task-manager.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'task-manager';
+  showLayout = false;
+  constructor(private taskService: TaskManagerService){
+
+    
+
+  }
 }
