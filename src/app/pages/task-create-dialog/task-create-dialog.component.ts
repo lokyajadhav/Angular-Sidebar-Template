75e1ae186d3fd0530abac50cffc6a42c848604a4
lokyajadhav <lokya57@gmail.com>
2025-04-20 03:53:57 +0000

    Apr 20, 2025, 9:23 AM

--- src/app/pages/task-create-dialog/task-create-dialog.component.ts
+++ src/app/pages/task-create-dialog/task-create-dialog.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-task-create-dialog',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./task-create-dialog.component.css']
 })
 export class TaskCreateDialogComponent {
+  taskForm: FormGroup;
 
+  constructor(
+    public dialogRef: MatDialogRef<TaskCreateDialogComponent>,
+   
+    private fb: FormBuilder
+  ) {
+    this.taskForm = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onSubmit(): void {
+    if (this.taskForm.valid) {
+      const taskData = this.taskForm.value;
+      // Call API to save task here (e.g., taskService.createTask(taskData))
+      console.log('Task Created:', taskData);
+      this.dialogRef.close(taskData);
+    }
+  }
 }
